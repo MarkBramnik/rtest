418b906517e75bd34819c021a1fdaf89077c0ea6
Mark Bramnik <mark.bramnik@gmail.com>
2017-01-16 11:27:26 +0200

    Can run the full functional test now in a single maven build

--- rtest-webapp-testapp/rtest-web-testapp-webapp/src/main/java/org/rtest/testapp/webapp/server/embedded/main/Main.java
+++ rtest-webapp-testapp/rtest-web-testapp-webapp/src/main/java/org/rtest/testapp/webapp/server/embedded/main/Main.java
@@ -2,6 +2,7 @@ package org.rtest.testapp.webapp.server.embedded.main;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 import org.rtest.testapp.webapp.spring.beans.AppConfig;
 import org.springframework.web.context.ContextLoaderListener;
@@ -14,16 +15,38 @@ import java.io.File;
  * Created by Mark Bramnik on 08/11/2016.
  */
 public class Main {
-    public static void main(String[] args) throws ServletException, LifecycleException {
+    private void startTomcat() throws ServletException, LifecycleException{
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
-        Context context = tomcat.addWebapp("/", new File(".").getAbsolutePath());
+        // create an AJP connector
+        Connector ajpConnector = createAjpConnector();
+        tomcat.getService().addConnector(ajpConnector);
+        System.out.println("Tomcat is about to start for context: " + new File(".").getAbsolutePath());
+        Context context = tomcat.addWebapp("/sample-app", new File(".").getAbsolutePath());
+        configureWebAppContext(context);
+        tomcat.start();
+        System.out.println("Tomcat Started...");
+        tomcat.getServer().await();
+    }
+
+    private void configureWebAppContext(Context context) throws ServletException {
         context.addParameter("contextClass", AnnotationConfigWebApplicationContext.class.getName());
         context.addParameter("contextConfigLocation", AppConfig.class.getName());
         context.addApplicationListener(ContextLoaderListener.class.getName());
         context.addApplicationListener(RTestIntegrationListener.class.getName());
-        tomcat.start();
-        System.out.println("Tomcat Started...");
-        tomcat.getServer().await();
+        context.setParentClassLoader(Main.class.getClassLoader());
+    }
+
+    private Connector createAjpConnector() {
+        Connector ajpConnector = new Connector("AJP/1.3");
+        ajpConnector.setPort(8009);
+        ajpConnector.setRedirectPort(8080);
+        ajpConnector.setSecure(false);
+        return ajpConnector;
+    }
+
+    public static void main(String[] args) throws ServletException, LifecycleException {
+       Main main = new Main();
+       main.startTomcat();
     }
 }
